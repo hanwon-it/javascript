a90df5d5344819d64b71888d69083a0e8a9172b0
hanwon <hanwon000128@gmail.com>
2025-04-11 16:48:18 +0900

    가위바위보_및_랜덤숫자_설명추가

--- gawibawibo.js
+++ gawibawibo.js
@@ -1,7 +1,12 @@
 let user = "보";
-
+// 1 ~ 3 사이에 숫자 발생하도록 설정
 const randex = Math.floor(Math.random() * 3) + 1;
 
+// 생성된 랜덤 숫자의 값이 맞는 if문으로 들어가고
+// 그 값과 user의 값이 같은 if문의 경우를
+// 찾아 승 패 무 결과 출력
+
+// 발생한 랜덤 숫자와 user의 값이 모두 같을 경우 결과 출력
 if (randex === 1) {
   console.log("컴퓨터는 가위를 선택했습니다");
   if (randex === 1 && user === "가위") {
--- randomnumber.js
+++ randomnumber.js
@@ -4,11 +4,15 @@
 // console.log(arr);
 
 const randomnumber = [];
+// 랜덤 숫자 6개 생성을 위해 배열이 6개가 되도록 반복
 while (randomnumber.length < 6) {
+  // 1 ~ 45 사이에 숫자가 발생하도록 설정
   const randex = Math.floor(Math.random() * 45) + 1;
+  // 숫자 중복 방지
   if (!randomnumber.includes(randex)) {
     randomnumber.push(randex);
   }
 }
+// 숫자 기반 오름차순 정렬-> .sort((a, b) => a - b);
 randomnumber.sort((a, b) => a - b);
 console.log(randomnumber);
